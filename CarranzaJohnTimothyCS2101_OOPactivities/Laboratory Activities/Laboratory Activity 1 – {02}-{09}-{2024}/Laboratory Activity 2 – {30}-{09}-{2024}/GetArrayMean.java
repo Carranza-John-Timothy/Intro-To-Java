47d0d0d6bb00246e2f16a2fa4e3537901b54559f
Carranza-John-Timothy <157569639+Carranza-John-Timothy@users.noreply.github.com>
2024-09-30 09:23:22 +0800

    Update GetArrayMean.java

--- CarranzaJohnTimothyCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 1 – {02}-{09}-{2024}/Laboratory Activity 2 – {30}-{09}-{2024}/GetArrayMean.java
+++ CarranzaJohnTimothyCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 1 – {02}-{09}-{2024}/Laboratory Activity 2 – {30}-{09}-{2024}/GetArrayMean.java
@@ -1,35 +1,27 @@
 import java.util.Scanner;
 
 public class GetArrayMean {
-    public GetArrayMean() {
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number of Elements: ");
-        int n = scanner.nextInt();
-        int[] array = new int[n];
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter number of Elements: ");
+            int n = scanner.nextInt();
+            int[] array = new int[n];
 
-        for(int i = 0; i < n; ++i) {
-            System.out.print("Enter Number: ");
-            array[i] = scanner.nextInt();
+            for(int i = 0; i < n; ++i) {
+                System.out.print("Enter Number: ");
+                array[i] = scanner.nextInt();
+            }
+
+            double mean = getArrayMean(array);
+            System.out.println("Mean of array is: " + mean);
         }
-
-        double mean = getArrayMean(array);
-        System.out.println("Mean of array is: " + mean);
     }
 
     public static double getArrayMean(int[] array) {
         int sum = 0;
-        int[] var2 = array;
-        int var3 = array.length;
-
-        for(int var4 = 0; var4 < var3; ++var4) {
-            int num = var2[var4];
+        for (int num : array) {
             sum += num;
         }
-
-        double mean = (double)sum / (double)array.length;
-        return mean;
+        return (double) sum / array.length;
     }
 }
