f1942a1ce3d4c2acd25b2a94ab7dc7a4b417c7a3
Carranza-John-Timothy <157569639+Carranza-John-Timothy@users.noreply.github.com>
2024-09-30 09:18:22 +0800

    Update MultiplicationTable.java

--- CarranzaJohnTimothyCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 1 – {02}-{09}-{2024}/Laboratory Activity 2 – {30}-{09}-{2024}/MultiplicationTable.java
+++ CarranzaJohnTimothyCS2101_OOPactivities/Laboratory Activities/Laboratory Activity 1 – {02}-{09}-{2024}/Laboratory Activity 2 – {30}-{09}-{2024}/MultiplicationTable.java
@@ -6,22 +6,31 @@ public class MultiplicationTable {
             System.out.print("Enter the size of the multiplication table: ");
             int maxSize = scanner.nextInt();
 
-            int[][] table = new int[maxSize][maxSize];
+            int[][] table = fillMultiplicationTable(maxSize);
+            printMultiplicationTable(table);
+        }
+    }
 
-            for (int i = 1; i <= maxSize; i++) {
-                for (int j = 1; j <= maxSize; j++) {
-                    table[i-1][j-1] = i * j;
-                }
+    public static int[][] fillMultiplicationTable(int size) {
+        int[][] table = new int[size][size];
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i-1][j-1] = i * j;
             }
+        }
 
-            System.out.println("Multiplication Table:");
+        return table;
+    }
 
-            for (int i = 0; i < maxSize; i++) {
-                for (int j = 0; j < maxSize; j++) {
-                    System.out.printf("%4d", table[i][j]);
-                }
-                System.out.println();
+    public static void printMultiplicationTable(int[][] table) {
+        System.out.println("Multiplication Table:");
+1
+        for (int[] ints : table) {
+            for (int anInt : ints) {
+                System.out.printf("%4d", anInt);
             }
+            System.out.println();
         }
     }
 }
